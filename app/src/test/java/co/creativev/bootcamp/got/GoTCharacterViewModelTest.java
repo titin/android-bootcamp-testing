8773ef8907e72416d1893af78f43888c35f7636f
Sumit Das <sumitdas.usit@gmail.com>
2016-01-27 14:26:23 +0700

    Added unit tests on character view model for alive/dead color

--- app/src/test/java/co/creativev/bootcamp/got/GoTCharacterViewModelTest.java
+++ app/src/test/java/co/creativev/bootcamp/got/GoTCharacterViewModelTest.java
@@ -1,6 +1,7 @@
 package co.creativev.bootcamp.got;
 
-import org.junit.After;
+import android.graphics.Color;
+
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -11,5 +12,19 @@ public class GoTCharacterViewModelTest {
         GoTCharacter gotCharacter = new GoTCharacter("Sansa", "Stark", "", true, "", 0, "", "");
         GoTCharacterViewModel gotCharacterViewModel = new GoTCharacterViewModel(gotCharacter);
         assertEquals("Sansa Stark", gotCharacterViewModel.name());
+    }
+
+    @Test
+    public void testReturnsRedWhenCharacterIsDead() throws Exception {
+        GoTCharacter gotCharacter = new GoTCharacter("", "", "", false, "", 0, "", "");
+        GoTCharacterViewModel gotCharacterViewModel = new GoTCharacterViewModel(gotCharacter);
+        assertEquals(Color.RED, gotCharacterViewModel.color());
+    }
+
+    @Test
+    public void testReturnsGreenWhenCharacterIsAlive() throws Exception {
+        GoTCharacter gotCharacter = new GoTCharacter("", "", "", true, "", 0, "", "");
+        GoTCharacterViewModel gotCharacterViewModel = new GoTCharacterViewModel(gotCharacter);
+        assertEquals(Color.GREEN, gotCharacterViewModel.color());
     }
 }
