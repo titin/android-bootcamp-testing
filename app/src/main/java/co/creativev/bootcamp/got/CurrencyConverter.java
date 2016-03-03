847d6d16d41188145044efbb5602292dbda328c5
Titin Kurniati <titin@go-jek.com>
2016-03-03 12:23:19 +0700

    [Titin] add function conver 1IDR to 0.05INR, 100IDR to 0.5INR

--- app/src/main/java/co/creativev/bootcamp/got/CurrencyConverter.java
+++ app/src/main/java/co/creativev/bootcamp/got/CurrencyConverter.java
@@ -2,12 +2,13 @@ package co.creativev.bootcamp.got;
 
 
 public class CurrencyConverter {
-    public int currencyConverter(int amount, String s) {
+    public double currencyConverter(double amount, String s) {
         if (s.equals("INR")){
             return amount*200;
         } else {
             return amount/200;
         }
     }
+
 
 }
--- app/src/test/java/co/creativev/bootcamp/got/CurrencyConverterTest.java
+++ app/src/test/java/co/creativev/bootcamp/got/CurrencyConverterTest.java
@@ -11,23 +11,28 @@ public class CurrencyConverterTest {
     @Test
     public void convert1INRto200IDR() throws IOException{
         CurrencyConverter currencyConverter = new CurrencyConverter();
-        int amountCurrency = currencyConverter.currencyConverter(1, "INR");
-        assertEquals(200,amountCurrency);
+        double amountCurrency = currencyConverter.currencyConverter(1, "INR");
+        assertEquals(200,amountCurrency,0.0);
     }
 
     @Test
     public void convert2INRto400IDR() throws IOException{
         CurrencyConverter currencyConverter = new CurrencyConverter();
-        int amountCurrency = currencyConverter.currencyConverter(2, "INR");
-        assertEquals(400,amountCurrency);
+        double amountCurrency = currencyConverter.currencyConverter(2, "INR");
+        assertEquals(400,amountCurrency,0.0);
     }
 
     @Test
     public void convert200IDRto1INR() throws IOException{
         CurrencyConverter currencyConverter = new CurrencyConverter();
-        int amountCurrency = currencyConverter.currencyConverter(200, "IDR");
-        assertEquals(1,amountCurrency);
+        double amountCurrency = currencyConverter.currencyConverter(200, "IDR");
+        assertEquals(1,amountCurrency,0.0);
     }
 
-
+    @Test
+    public void convertIDRtoINR() throws Exception {
+        CurrencyConverter currencyConverter = new CurrencyConverter();
+        double amountCurrency = currencyConverter.currencyConverter(1, "IDR");
+        assertEquals(0.05,amountCurrency,0.0);
+    }
 }
